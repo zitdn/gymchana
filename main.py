4f2a17b914abe6c804331a8ff68905bed96acb94
Zitm <zitonovdmitriy@gmail.com>
2026-07-20 23:15:48 +0400

    add pyside

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello')
+from PySide6.QtWidgets import QApplication
+from ui.main_window import MainWindow
+import sys
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    return app.exec()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
