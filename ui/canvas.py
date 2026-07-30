b03f4836396b61292a25423d20fc5eb3fdf4837d
Zitdn <zitmalay@gmail.com>
2026-07-30 13:21:04 +0400

    add painter

--- ui/canvas.py
+++ ui/canvas.py
@@ -1,7 +1,11 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QPainter
 
 
 class Canvas(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         pass
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.fill
